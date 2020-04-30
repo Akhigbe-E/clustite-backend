635fa5a41b427657750c7fdb0605aa95c50a3c9a
Emmanuel <akhigbemmanuel@gmai.com>
2020-04-30 09:09:39 +0100

    Implementing datasource

--- src/datasources/clustite-database.js
+++ src/datasources/clustite-database.js
@@ -13,17 +13,44 @@ class ClustiteDatabase extends SQLDataSource {
     getIDOfMembersOfCommitment(commitmentGroupID) {
         return this.knex.select('user_id').from('user_commitment_ids').where({ commitment_group_id: commitmentGroupID })
     }
-    getUsers(userIDs) {
-        console.log(userIDs)
-        return this.knex.select('*').from('users').whereIn({ id: [...userIDs] })
+    getUsers({ user_id }) {
+        console.log(user_id)
+        return this.knex.select('*').from('users').whereIn('id', [user_id])
     }
     getCommitmentGroupMembers(commitmentGroupID) {
         console.log(commitmentGroupID)
-        return this.getIDOfMembersOfCommitment(commitmentGroupID).then(userIDs => this.getUsers(userIDs))
+        return this.getIDOfMembersOfCommitment(commitmentGroupID)
+            .then(userIDs => this.getUsers(...userIDs))
+            .then(data => {
+                return Array.isArray(data) ? data.map(data => this.userReducer(data)) : []
+            })
     }
 
     getClusters(commitmentID) {
-        return this.knex.select('*').from('clusters').where({ commitment_group_id: commitmentID })
+        return this.knex.select('*')
+            .from('clusters')
+            .where({ commitment_group_id: commitmentID })
+            .then(data => Array.isArray(data) ? data.map(data => this.clusterReducer(data)) : [])
+    }
+
+    clusterReducer({ id, commitment_group_id, cluster_name, cluster_score, reward }) {
+        return {
+            id,
+            clusterName: cluster_name,
+            clusterScore: cluster_score,
+            reward
+            // Stopped HERE!!
+        }
+    }
+
+    userReducer({ id, username, matric_number, account_number, has_paid }) {
+        return {
+            id,
+            name: username,
+            matricNumber: matric_number,
+            accountNumber: account_number,
+            hasPaid: has_paid
+        }
     }
 
     commitmentGroupReducer({ id, group_name, group_type, group_joining_code, commitment_name, commitment_description, stake }) {

028567fde3f94c32e8c128a3df495a5c618ccca3
Emmanuel <akhigbemmanuel@gmai.com>
2020-05-05 04:26:46 +0100

    Added Cors

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schemas/schema')
 const resolvers = require('./resolvers')
 const knex = require("knex");
+const express = require('express')
 // const redis = require('redis')
 const redis = require("async-redis");
 
 const ClustiteDatabase = require('./datasources/clustite-database')
 
-const getAuthId = require('./auth')
-
+const app = express();
 
 const redisClient = redis.createClient(process.env.REDIS_URI)
 
@@ -21,6 +21,10 @@ const knexInstance = knex({
 
 const db = new ClustiteDatabase(knexInstance, redisClient);
 
+const corsOption = {
+    origin: '*',
+    credentials: true
+}
 
 const server = new ApolloServer({
     typeDefs,
@@ -35,6 +39,7 @@ const server = new ApolloServer({
     })
 })
 
+server.applyMiddleware({ app, cors: corsOption })
 
 server.listen().then(({ url }) => {
     console.log(`🚀 Server ready at ${url}`);
